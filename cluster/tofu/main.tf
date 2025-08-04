cdf11ee01144268611b99e825a4938089e2cf4aa
Maximilien Nadji <maximilien.nadji@pm.me>
2025-08-04 09:07:02 +0000

    setup secret encryption to mask ip address and username (#3)

--- cluster/tofu/main.tf
+++ cluster/tofu/main.tf
@@ -1,7 +1,21 @@
+data "sops_file" "secrets" {
+    source_file = "${path.module}/secrets/secrets.enc.yml"
+}
+
+locals {
+    node_secrets = yamldecode(data.sops_file.secrets.raw)
+    nodes = length(local.node_secrets.nodes) > 0 ? [for n in local.node_secrets.nodes : {
+      name = n.name
+      ip   = n.ip
+      role = n.role
+      user = n.user
+  }] : var.nodes
+}
+
 resource "local_file" "ansible_inventory" {
     filename = "${path.module}/../ansible/inventory/hosts.yml"
 
     content = templatefile("${path.module}/templates/hosts.yml.tpl", {
-        nodes = var.nodes
+        nodes = local.nodes
     })
 }
