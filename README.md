9ef01cd020bdedd284bf9e6b2313eb86783893e2
nadmax <maximilien.nadji@pm.me>
2025-08-04 11:25:40 +0200

    setup CI
    - Add CI badge in the README
    - Run tofu format check and tofu config validation on PR
    - Run tofu plan and tofu apply on master branch

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+![CI](https://github.com/nadmax/website/actions/workflows/ci.yml/badge.svg)
 # Pi-Ops
--- cluster/tofu/providers.tf
+++ cluster/tofu/providers.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_version = "1.10.3"
-    required_providers {
-        local = {
-            source  = "hashicorp/local"
-            version = "2.5.3"
-        }
-        sops = {
-            source = "carlpett/sops"
-            version = "1.2.1"
-        }
+  required_version = ">= 1.10.3"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
     }
+    sops = {
+      source  = "carlpett/sops"
+      version = "1.2.1"
+    }
+  }
 }
