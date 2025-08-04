dc52a3dfcc0a88e2fdde45433fd54f503495c78b
Maximilien Nadji <maximilien.nadji@pm.me>
2025-08-04 10:55:14 +0000

    setup CI (#4)
    - Add CI badge in the README
    - Run tofu format check and tofu config validation on PR
    - Run tofu plan and tofu apply on master branch

--- cluster/tofu/outputs.tf
+++ cluster/tofu/outputs.tf
@@ -1,12 +1,12 @@
 output "nodes" {
-    value = local.nodes
-    sensitive = true
+  value     = local.nodes
+  sensitive = true
 }
 
 output "inventory_files" {
-    description = "Generated Ansible inventory file"
+  description = "Generated Ansible inventory file"
 
-    value = {
-        yaml_inventory = local_file.ansible_inventory.filename
-    }
+  value = {
+    yaml_inventory = local_file.ansible_inventory.filename
+  }
 }
